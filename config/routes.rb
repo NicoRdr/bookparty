37887bc04fbbbe5d4efc10a41c2942d609c0d311
NicoRdr <nicolas.rodrigues94@gmail.com>
2017-03-22 23:16:42 +0100

    modification root

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   # root to: 'visitors#index'
   #home page
-  root to: "welcome#index"
-  devise_for :users
-  resources :users
-
+  
+  
   #login
   authenticated :user do
   root to: 'evenings#index', as: :authenticated_root
   end
+  root to: "welcome#index"
   
   #get 'evening' => 'evening#show'
 
+  devise_for :users
+  resources :users
   # resources :evenings, only:[:index, :show]
 end
