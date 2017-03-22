6dcf04cf391660f268bdbf5ebbc91f87074301e1
NicoRdr <nicolas.rodrigues94@gmail.com>
2017-03-22 21:35:37 +0100

    Add page Welcome et modification root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  # root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  #home page
+  root to: "welcome#index"
+  #login
+  authenticated :user do
+  root to: 'evenings#index', as: :authenticated_root
+  end
+  # root to: 'evenings#index'
+  #get 'evening' => 'evening#show'
+
+  # resources :evenings, only:[:index, :show]
 end
