74a026568edcdb11714ca38260eb039b6a5a0927
NicoRdr <nicolas.rodrigues94@gmail.com>
2017-03-22 14:04:35 +0100

    remplace line for route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # root to: 'visitors#index'
-  # devise_for :users
-  # resources :users
+  root to: 'visitors#index'
+  devise_for :users
+  resources :users
+  resources :evenings, only:[:index, :show]
 
-  authenticated :user do
-  root to: 'evenings#index', as: :authenticated_root
-  end
-  root to: "users#index"
+  # authenticated :user do
+  # root to: 'evenings#index', as: :authenticated_root
+  # end
+  # root to: "users#index"
 
 end
