2b0c0625e555f110e194fdd31699c6be3d09a5d5
NicoRdr <nicolas.rodrigues94@gmail.com>
2017-03-22 13:52:00 +0100

    create folder evening for appli and modifed route add evening seed example

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
-  devise_for :users
-  resources :users
+  # root to: 'visitors#index'
+  # devise_for :users
+  # resources :users
+
+  authenticated :user do
+  root to: 'evenings#index', as: :authenticated_root
+  end
+  root to: "users#index"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+
+###### seed evening for exemple (Soiree) #######
+Soiree.create( nom: 'Mousse',
+              date: Date.new(2017, 12, 10),
+              description: 'Soirée mousse, apportez vos maillots de bain et serviettes !',
+              place: 80,
+              price: 10,
+              adress: 'Paris',
+              type: 'DanceFloor')
+
+@annee = Soiree.create( nom: 'Année 80',
+              date: Date.new(2017, 04, 06),
+              description: 'Revivez les années 80 sans la coupe de cheveux !',
+              place: 80,
+              price: 80,
+              adress: 'Chartres',
+              type: 'Retro' )
+
+Soiree.create( nom: 'Jour de l\'an',
+              date: Date.new(2017, 12, 31),
+              description: 'Fetez la nouvelle année avec la Companie Créole et Kassav',
+              place: 100,
+              price: 50,
+              adress: 'La Loupe',
+              type: 'Déguisée' )
+
+Soiree.create( nom: 'Electro',
+              date: Date.new(2017, 06, 03),
+              description: 'Soirée remix music...',
+              place: 200,
+              price: 30,
+              adress: 'Roubaix',
+              type: 'Beuverie' )
