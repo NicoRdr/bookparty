0369ec835c306bff6f5bf53125dcdbb344b2eef8
NicoRdr <nicolas.rodrigues94@gmail.com>
2017-03-22 22:42:38 +0100

    Modifed routes for acess page login authenticated user link to evening list page index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # root to: 'visitors#index'
+  #home page
+  root to: "welcome#index"
   devise_for :users
   resources :users
 
-  #home page
-  root to: "welcome#index"
   #login
   authenticated :user do
   root to: 'evenings#index', as: :authenticated_root
   end
-  # root to: 'evenings#index'
+  
   #get 'evening' => 'evening#show'
 
   # resources :evenings, only:[:index, :show]
